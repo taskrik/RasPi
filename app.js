f010163f392fa70003e2563dd0f592c2bce3e2b5
taskrik <tasos.krikonis@gmail.com>
2019-01-14 14:50:42 +0100

    Creates on and off end points

--- app.js
+++ app.js
@@ -3,23 +3,29 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
-const gpio = require('rpi-gpio');
-gpio.setup(7,gpio.DIR_OUT)
+const gpio = require("rpi-gpio");
+gpio.setup(7, gpio.DIR_OUT);
 
-
+//testing the EndPoints
 app.get("/", (req, res) => res.send("hello there from the server"));
 app.get("/next-page", (req, res) =>
   res.json({ page: "Next Page", page1: "blasd" })
 );
 
-app.post('/led/on', function(req, res){
-    gpio.write(7, true, function(err) {
-            if (err) throw err;
-            console.log('Written True to pin');
-        console.log(path.join(__dirname, 'public'));
-        return res.render('index', {status: "Cool!!Led is On"});
-        });
-    
-    });
+//turn led on
+app.post("/led/on", function(req, res) {
+  gpio.write(7, true, function(err) {
+    if (err) throw err;
+    console.log("Written True to pin");
+  });
+});
+
+//turn led off
+app.post("/led/off", function(req, res) {
+  gpio.write(7, false, function(err) {
+    if (err) throw err;
+    console.log("Written False to pin");
+  });
+});
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
