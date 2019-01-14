bbd7c5c57b0f29a4d7ee337bbdfa3ecbd166457f
taskrik <tasos.krikonis@gmail.com>
2019-01-14 14:56:52 +0100

    Renders to the client side

--- app.js
+++ app.js
@@ -3,8 +3,21 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+const path = require('path');
+
 const gpio = require("rpi-gpio");
 gpio.setup(7, gpio.DIR_OUT);
+
+
+app.set('view engine', 'ejs');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+console.log(path.join(__dirname, 'public'));
+
+app.get('/', function(req, res){ 
+ 	res.render('index',{status:"Press Button To change Status of Led !!"});
+});
 
 //testing the EndPoints
 app.get("/", (req, res) => res.send("hello there from the server"));
@@ -17,6 +30,8 @@ app.post("/led/on", function(req, res) {
   gpio.write(7, true, function(err) {
     if (err) throw err;
     console.log("Written True to pin");
+    console.log(path.join(__dirname, 'public'));
+	return res.render('index', {status: "Cool!!Led is On"});
   });
 });
 
@@ -25,6 +40,8 @@ app.post("/led/off", function(req, res) {
   gpio.write(7, false, function(err) {
     if (err) throw err;
     console.log("Written False to pin");
+    console.log(path.join(__dirname, 'public'));
+	return res.render('index',{status: "Ohh!! Led is Off"});
   });
 });
 
