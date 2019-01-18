b2b5ca86e11c39b93594b59f0851ea4a0ee519af
taskrik <tasos.krikonis@gmail.com>
2019-01-18 12:02:46 +0100

    Setting up the leds the buzzer and the sensor

--- app.js
+++ app.js
@@ -6,8 +6,28 @@ const port = 3000;
 const path = require("path");
 
 const gpio = require("rpi-gpio");
+//setup single led
 gpio.setup(7, gpio.DIR_OUT);
-gpio.setup(29, gpio.DIR_OUT)
+
+//setup alarm system
+//buzzer
+gpio.setup(29, gpio.DIR_OUT);
+
+//leds
+gpio.setup(12, gpio.DIR_OUT);
+gpio.setup(16, gpio.DIR_OUT);
+gpio.setup(18, gpio.DIR_OUT);
+
+//gpio.setMode(gpio.MODE_RPI);
+
+//sensor
+let sensor = {
+  pin: 15,
+  loopTime: 1500,
+  tripped: false,
+  value: undefined
+};
+
 
 app.set("view engine", "ejs");
 
@@ -41,18 +61,42 @@ app.post("/led/off", function(req, res) {
 
 //turn alarm on
 app.post("/alarm/on", function(req, res) {
-  gpio.write(29, true, function(err) {
-    if (err) throw err;
-    return res.render("index", { status: "Alarm is on" });
-  });
+  //setting up the sensor
+  gpio.setup(sensor.pin, gpio.DIR_IN, onSetup);
+  
+  const onSetup = (error) => {
+    if (error) console.error(error);
+    return setInterval(readInterval, sensor.loopTime);
+  }
+  
+  const readInterval = function() {
+    gpio.read(sensor.pin, function(error, value) {
+      if (value === sensor.tripped) return (sensor.tripped = value);
+      if (sensor.tripped) console.log("We have movement!");
+      else console.log("it's quiet... a little TOO quiet...");
+    });
+  };
+  if (readInterval) {
+    gpio.write(12, true);
+    gpio.write(16, true);
+    gpio.white(18, true);
+    gpio.write(29, true, function(err) {
+      if (err) throw err;
+      return res.render("index", { status: "Alarm is on" });
+    });
+  }
 });
+
 
 //turn alarm off
 app.post("/alarm/off", function(req, res) {
-  gpio.write(29, false, function(err) {
-    if (err) throw err;
-    return res.render("index", { status: "Alarm is on" });
-  });
+  gpio.write(12, false);
+    gpio.write(16, false);
+    gpio.white(18, false);
+    gpio.write(29, false, function(err) {
+      if (err) throw err;
+      return res.render("index", { status: "Alarm is off" });
+    });
 });
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
