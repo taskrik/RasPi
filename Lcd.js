0e53faf049c28504db833d3d23853db09ab53294
taskrik <tasos.krikonis@gmail.com>
2019-01-19 22:00:52 +0100

    Keep testing

--- Lcd.js
+++ Lcd.js
@@ -3,9 +3,10 @@ const Lcd = require('lcd');
 
 
 //LCD screen
-const lcd = new Lcd({rows: 1});
+const lcd = new Lcd({rows: 2, cols: 20});
 
 lcd.on('ready', () => {
+    lcd.cursor(1,0)
     lcd.print("Hello world", (err) => {
       if (err) {
         throw err;
