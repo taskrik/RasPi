1d0d23fe1ba9f726a0af66c51e4ba0c40e19a958
taskrik <tasos.krikonis@gmail.com>
2019-01-19 22:05:48 +0100

    Remove throw error

--- Lcd.js
+++ Lcd.js
@@ -5,12 +5,7 @@ const lcd = new Lcd({ rows: 2, cols: 20 });
 
 lcd.on("ready", () => {
   lcd.cursor(0, 0);
-  lcd.print("Hello world", err => {
-    if (err) {
-      console.log("the error", err);
-      throw err;
-    }
-  });
+  lcd.print("Hello world");
 });
 
 process.on("SIGINT", () => {
