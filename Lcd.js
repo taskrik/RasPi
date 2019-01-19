51b2e32cc9a3ba791c5763d012792e7c66eb410c
taskrik <tasos.krikonis@gmail.com>
2019-01-19 21:57:46 +0100

    Testing changes

--- Lcd.js
+++ Lcd.js
@@ -3,18 +3,16 @@ const Lcd = require('lcd');
 
 
 //LCD screen
-const lcd = new Lcd({rows: 2});
+const lcd = new Lcd({rows: 1});
 
 lcd.on('ready', () => {
-  setInterval(() => {
-    lcd.setCursor(0, 0);
     lcd.print("Hello world", (err) => {
       if (err) {
         throw err;
       }
     });
-  }, 1000);
-});
+  }
+);
 
 process.on('SIGINT', () => {
     lcd.close();
