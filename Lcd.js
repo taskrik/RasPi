f05e74a74c70b9d36754317e6dee87bd301ce4f2
taskrik <tasos.krikonis@gmail.com>
2019-01-19 22:03:46 +0100

    nothing yet

--- Lcd.js
+++ Lcd.js
@@ -1,21 +1,19 @@
-const Lcd = require('lcd');
-
-
+const Lcd = require("lcd");
 
 //LCD screen
-const lcd = new Lcd({rows: 2, cols: 20});
+const lcd = new Lcd({ rows: 2, cols: 20 });
 
-lcd.on('ready', () => {
-    lcd.cursor(1,0)
-    lcd.print("Hello world", (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-  }
-);
-
-process.on('SIGINT', () => {
-    lcd.close();
-    process.exit();
+lcd.on("ready", () => {
+  lcd.cursor(0, 0);
+  lcd.print("Hello world", err => {
+    if (err) {
+      console.log("the error", err);
+      throw err;
+    }
   });
+});
+
+process.on("SIGINT", () => {
+  lcd.close();
+  process.exit();
+});
