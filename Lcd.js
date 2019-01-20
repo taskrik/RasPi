d400a704d72539e1d81d652aefe3a33f020e7234
taskrik <tasos.krikonis@gmail.com>
2019-01-20 01:10:50 +0100

    Try new lib

--- Lcd.js
+++ Lcd.js
@@ -1,14 +1,24 @@
-const Lcd = require("lcd");
+// const Lcd = require("lcd");
 
-//LCD screen
-const lcd = new Lcd({ rows: 2, cols: 20 });
+// //LCD screen
+// const lcd = new Lcd({ rows: 2, cols: 20 });
 
-lcd.on("ready", () => {
-  lcd.cursor(0, 0);
-  lcd.print("Hello world");
-});
+// lcd.on("ready", () => {
+//   lcd.cursor(0, 0);
+//   lcd.print("Hello world");
+// });
 
-process.on("SIGINT", () => {
-  lcd.close();
-  process.exit();
+// process.on("SIGINT", () => {
+//   lcd.close();
+//   process.exit();
+// });
+
+const five = require("johnny-five");
+const board = new five.Board();
+
+board.on("ready", function() {
+
+  var lcd = new five.LCD({ pins: [7, 8, 9, 10, 11, 12] });
+
+  lcd.print("Hello");
 });
