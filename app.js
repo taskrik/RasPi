015d9acfe2d8c2225598f577e651bf219ce73de9
taskrik <tasos.krikonis@gmail.com>
2019-01-26 22:20:37 +0100

    Remove old code and apply Johnny5

--- app.js
+++ app.js
@@ -1,36 +1,25 @@
 const express = require("express");
+const five = require("johnny-five");
+const Raspi = require("raspi-io");
+
+const board = new five.Board({
+  io: new Raspi()
+});
 
 const app = express();
 const port = 3000;
 
 const path = require("path");
 
-const gpio = require("rpi-gpio");
-const Gpio = require("onoff").Gpio
- 
+// //setup alarm system
 
-//setup single led
-const singleLed = new Gpio(4, "out");
+// //buzzer
+// const buzzer = new Gpio(5, "out")
 
-
-//setup alarm system
-
-//buzzer
-const buzzer = new Gpio(5, "out")
-
-//leds
-gpio.setup(12, gpio.DIR_OUT);
-gpio.setup(16, gpio.DIR_OUT);
-gpio.setup(18, gpio.DIR_OUT);
-
-
-//sensor
-let sensor = {
-  pin: 22
-};
-
-const pirSensor = new Gpio(sensor.pin, "in", "both")
-
+// //leds
+// gpio.setup(12, gpio.DIR_OUT);
+// gpio.setup(16, gpio.DIR_OUT);
+// gpio.setup(18, gpio.DIR_OUT);
 
 app.set("view engine", "ejs");
 
@@ -42,68 +31,68 @@ app.get("/", function(req, res) {
   res.render("index", { status: "Press Button To change Status of Led !!" });
 });
 
-//turn led on
-app.post("/led/on", function(req, res) {
-  
-  singleLed.writeSync(1)
-  return res.render("index", { status: "Cool!!Led is On" });
+board.on("ready", function() {
+  const led = new five.Led(7);
+  const motion = new five.Motion(13, {
+    controller: "PIR"
   });
 
+  //turn led on
+  app.post("/led/on", function(req, res) {
+    led.on();
+    console.log("led is on");
 
-//turn led off
-app.post("/led/off", function(req, res) {
-    singleLed.writeSync(0)
+    return res.render("index", { status: "Cool!!Led is On" });
+  });
+
+  //turn led off
+  app.post("/led/off", function(req, res) {
+    led.off();
+    console.log("led is off");
+
     return res.render("index", { status: "Ohh!! Led is Off" });
+  });
+
+  //turn alarm on
+  //   app.post("/alarm/on", function(req, res) {
+  //     res.render("index", { status: "Alarm is on" });
+  motion.on("calibrated", function() {
+    console.log("calibrated");
+  });
+  motion.on("motionstart", function() {
+    console.log("motionstart");
+  });
+
+  motion.on("motionend", function() {
+    console.log("motionend");
+  });
+
+  this.on("exit", function() {
+    led.off();
+  });
 });
 
-//turn alarm on
-app.post("/alarm/on", function(req, res) {
-	res.render("index", { status: "Alarm is on" });
-	pirSensor.watch(function(err,value){
-	if(value === 1){
-		console.log("Movement detected!", value)
-		gpio.write(12, true);
-  		gpio.write(16, true);
-  		gpio.write(18, true);
-		const iv = setInterval(() => buzzer.writeSync(value),200)
-  		setTimeout(() =>{ 
-		clearInterval(iv)
-		buzzer.writeSync(0) 
-}, 1000)
-	}else {
-	console.log("Area is clear!")
-	gpio.write(12, false);
-  	gpio.write(16, false);
-  	gpio.write(18, false);
-	buzzer.writeSync(0)
-	
-} 
-	})	
-	
+// this.on("exit", function() {
+//   led.off();
+// });
+// });
 
-});
+// //turn alarm off
+// app.post("/alarm/off", function(req, res) {
+//   res.render("index", { status: "You terminated the programme" });
 
+// });
 
-//turn alarm off
-app.post("/alarm/off", function(req, res) {
-  res.render("index", { status: "You terminated the programme" });
- 
-	
-});
+//   process.on("SIGINT", function(){
+//     console.log("You terminated the program")
+//     singleLed.writeSync(0);
+//     gpio.write(12, false);
+//     gpio.write(16, false);
+//     gpio.write(18, false);
+//     pirSensor.unexport()
+//     buzzer.unexport()
+//     process.exit()
 
-  process.on("SIGINT", function(){
-    console.log("You terminated the program")
-    singleLed.writeSync(0);
-    gpio.write(12, false);
-    gpio.write(16, false);
-    gpio.write(18, false);
-    pirSensor.unexport()
-    buzzer.unexport()
-    process.exit()
-
-})
-
-
-
+// })
 
 app.listen(port, () => console.log(`Listening to port ${port}`));
