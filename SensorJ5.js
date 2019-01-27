df8cb49f58bee06cb8fe8072f547de03f49fdc76
taskrik <tasos.krikonis@gmail.com>
2019-01-27 14:12:06 +0100

    Make a sensor file and test J5. It works!

---
+++ SensorJ5.js
@@ -0,0 +1,28 @@
+const five = require("johnny-five");
+const Raspi = require("raspi-io");
+
+const board = new five.Board({
+  io: new Raspi()
+});
+
+board.on("ready", function() {
+  // Create a new `motion` hardware instance.
+  const motion = new five.Motion(7);
+
+  // "calibrated" occurs once, at the beginning of a session,
+  motion.on("calibrated", function() {
+    console.log("calibrated");
+  });
+
+  // "motionstart" events are fired when the "calibrated"
+  // proximal area is disrupted, generally by some form of movement
+  motion.on("motionstart", function() {
+    console.log("motionstart");
+  });
+
+  // "motionend" events are fired following a "motionstart" event
+  // when no movement has occurred in X ms
+  motion.on("motionend", function() {
+    console.log("motionend");
+  });
+});
