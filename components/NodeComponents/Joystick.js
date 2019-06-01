94e3a37cbea8526c8dd9fde27fae272322947dce
taskrik <tasos.krikonis@gmail.com>
2019-06-01 23:30:46 +0200

    Creates a component for joystick trial

--- components/NodeComponents/Joystick.js
+++ components/NodeComponents/Joystick.js
@@ -1,15 +1,14 @@
 const five = require("johnny-five");
-const Raspi = require("raspi-io");
-var board = new five.Board();
+const Raspi = require("raspi-io").RaspiIO;
+const board = new five.Board({
+  io: new Raspi()
+});
 
 board.on("ready", function() {
   console.log("Board ready!");
 
   // Create a new `joystick` hardware instance.
-  const joystick = new five.Joystick({
-    //   [ x, y ]
-    pins: [11, 13]
-  });
+  const joystick = new five.Joystick(["GPIO17", "GPIO27"]);
 
   joystick.on("change", function() {
     console.log("Joystick");
